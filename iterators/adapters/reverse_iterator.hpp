1e8211304defe75427b428423e477b942363f083
Hovsep Hayrapetyan <hohayrap@c2r1s2.42yerevan.am>
2022-11-28 18:14:55 +0400

    fixed tabs

--- iterators/adapters/reverse_iterator.hpp
+++ iterators/adapters/reverse_iterator.hpp
@@ -15,12 +15,12 @@ namespace ft
 			/*    member types   */
 			/* ================= */
 
-			typedef Iter iterator_type;
-			typedef typename ft::iterator_traits<Iter>::iterator_category iterator_category;
-			typedef typename ft::iterator_traits<Iter>::value_type value_type;
-			typedef typename ft::iterator_traits<Iter>::difference_type difference_type;
-			typedef typename ft::iterator_traits<Iter>::pointer pointer;
-			typedef typename ft::iterator_traits<Iter>::reference reference;
+			typedef Iter													iterator_type;
+			typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
+			typedef typename ft::iterator_traits<Iter>::value_type			value_type;
+			typedef typename ft::iterator_traits<Iter>::difference_type		difference_type;
+			typedef typename ft::iterator_traits<Iter>::pointer				pointer;
+			typedef typename ft::iterator_traits<Iter>::reference			reference;
 
 
 			/* ========================= */
